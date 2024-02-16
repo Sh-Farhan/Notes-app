232d26b32ea8cdaf3286e996fa3c485b679422a8
Sh-Farhan <farhan.mh.shaikh@gmail.com>
2024-02-16 08:36:05 +0530

    done

--- main.js
+++ main.js
@@ -7,12 +7,27 @@ let notesDisplay = document.querySelector(".notes-display");
 let showOtherNotes = document.querySelector(".notes-container");
 let showPinnedNotes = document.querySelector(".pinned-notes-container");  
 
-let arrayOfNotes = [];
+let arrayOfNotes = JSON.parse(localStorage.getItem("notes")) || [];
+
+notesDisplay.addEventListener("click", (event) =>{
+    let type = event.target.dataset.type;
+    let noteId = event.target.dataset.id;
+
+    switch(type){
+        case "del" :
+            arrayOfNotes = arrayOfNotes.filter(({id}) =>  id.toString() !== noteId);
+            showOtherNotes.innerHTML = renderNotes(arrayOfNotes);
+            localStorage.setItem("notes",JSON.stringify(arrayOfNotes));
+    }
+})
 
 addNoteButton.addEventListener("click",()=> {
     if(note.value.trim().length > 0 || title.value.trim().length > 0){
         arrayOfNotes = [...arrayOfNotes,{id : Date.now(), title : title.value.trim(), note : note.value.trim(), isPinned : false, isArchived : false}];
+        note.value = title.value = "";
+        showOtherNotes.innerHTML = renderNotes(arrayOfNotes);
+        localStorage.setItem("notes",JSON.stringify(arrayOfNotes));
     }
+})  
 
-    console.log(arrayOfNotes);
-})
+showOtherNotes.innerHTML = renderNotes(arrayOfNotes)
